2065ada0181d609353e078f9a4c010f7e54e4b4d
John Dale <johnkdale02@gmail.com>
2023-04-01 14:41:03 -0400

    Model Update

--- human_pose_detection.py
+++ human_pose_detection.py
@@ -1,13 +1,15 @@
 import jetson_inference
 import jetson_utils
 
-net = jetson_inference.detectNet("densenet121-body", threshold=0.5)
+net = jetson_inference.poseNet("densenet121-body", threshold=0.5)
 # Createing net with pretrained densenet121-body human pose 
 camera = jetson_utils.gstCamera(1280, 720, "/dev/video0")
 display = jetson_utils.glDisplay()
 
 while display.IsOpen():
     img, width, height = camera.CaptureRGBA()
-    detections = net.Detect(img, width, height)
+    #image = camera.Capture()
+    poses = net.Process(img, overlay='box')
+
     display.RenderOnce(img, width, height)
-    display.SetTitle("Object Detection Network {:.0f} FPS".format(net.GetNetworkFPS()))  
+    display.SetTitle("Pose Detection Network {:.0f} FPS".format(net.GetNetworkFPS()))  
