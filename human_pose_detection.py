a7d7699b77294dc2ef4a4e69f712b23a7e6004ac
John Dale <johnkdale02@gmail.com>
2023-04-01 16:34:07 -0400

    Added details to pose information

--- human_pose_detection.py
+++ human_pose_detection.py
@@ -1,15 +1,61 @@
 import jetson_inference
 import jetson_utils
+import sys
+import argparse
 
-net = jetson_inference.poseNet("densenet121-body", threshold=0.5)
-# Createing net with pretrained densenet121-body human pose 
-camera = jetson_utils.gstCamera(1280, 720, "/dev/video0")
-display = jetson_utils.glDisplay()
+# parse the command line
+parser = argparse.ArgumentParser(description="Run pose estimation DNN on a video/image stream.", 
+                                 formatter_class=argparse.RawTextHelpFormatter, 
+                                 epilog=poseNet.Usage() + videoSource.Usage() + videoOutput.Usage() + Log.Usage())
 
-while display.IsOpen():
-    img, width, height = camera.CaptureRGBA()
-    #image = camera.Capture()
-    poses = net.Process(img, overlay='box')
+parser.add_argument("input", type=str, default="", nargs='?', help="URI of the input stream")
+parser.add_argument("output", type=str, default="", nargs='?', help="URI of the output stream")
+parser.add_argument("--network", type=str, default="resnet18-body", help="pre-trained model to load (see below for options)")
+parser.add_argument("--overlay", type=str, default="links,keypoints", help="pose overlay flags (e.g. --overlay=links,keypoints)\nvalid combinations are:  'links', 'keypoints', 'boxes', 'none'")
+parser.add_argument("--threshold", type=float, default=0.15, help="minimum detection threshold to use") 
 
-    display.RenderOnce(img, width, height)
-    display.SetTitle("Pose Detection Network {:.0f} FPS".format(net.GetNetworkFPS()))  
+try:
+	args = parser.parse_known_args()[0]
+except:
+	print("")
+	parser.print_help()
+	sys.exit(0)
+
+# load the pose estimation model
+net = poseNet(args.network, sys.argv, args.threshold)
+
+# create video sources & outputs
+input = videoSource(args.input, argv=sys.argv)
+output = videoOutput(args.output, argv=sys.argv)
+
+# process frames until EOS or the user exits
+while True:
+    # capture the next image
+    img = input.Capture()
+
+    if img is None: # timeout
+        continue  
+
+    # perform pose estimation (with overlay)
+    poses = net.Process(img, overlay=args.overlay)
+
+    # print the pose results
+    print("detected {:d} objects in image".format(len(poses)))
+
+    for pose in poses:
+        print(pose)
+        print(pose.Keypoints)
+        print('Links', pose.Links)
+
+    # render the image
+    output.Render(img)
+
+    # update the title bar
+    output.SetStatus("{:s} | Network {:.0f} FPS".format(args.network, net.GetNetworkFPS()))
+
+    # print out performance info
+    net.PrintProfilerTimes()
+
+    # exit on input/output EOS
+    if not input.IsStreaming() or not output.IsStreaming():
+        break
